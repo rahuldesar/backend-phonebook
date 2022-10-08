a4078ed7ffb0fbc414b9b031a1502b016cea5a67
Rahul Desar <desarrahul@gmail.com>
2022-10-08 12:23:55 +0545

    some linting-fixes

--- models/phonebook.js
+++ models/phonebook.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const url = process.env.MONGODB_URI;
-console.log("connecting to ", url);
+console.log('connecting to ', url);
 
 mongoose.connect(url)
-  .then(result =>{
-    console.log("Connected to MongoDB");
+  .then(result => {
+    console.log('Connected to MongoDB');
   })
-  .catch((error) => console.log("ERROR CONNECTING TO MONGODB", error));
+  .catch((error) => console.log('ERROR CONNECTING TO MONGODB', error));
 
 
 const phonebookSchema = new mongoose.Schema({
-  name : { 
+  name : {
     type: String,
     minLength :3,
     required : true,
@@ -33,15 +33,15 @@ const phonebookSchema = new mongoose.Schema({
     }
   }
 
-})
+});
 
 phonebookSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-})
+});
 
 module.exports = mongoose.model('Phonebook', phonebookSchema);
 
